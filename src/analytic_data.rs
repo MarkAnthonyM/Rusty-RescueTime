e7ca94febb875c769e0a0487df67a5ecde3c6a72
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-04 23:53:39 -0500

    Implement prototype RescueTime struct

--- src/analytic_data.rs
+++ src/analytic_data.rs
@@ -57,6 +57,11 @@ pub struct AnalyticData {
 
 }
 
+struct RescueTime {
+    perspective: Option<String>,
+    resolution: Option<String>,
+}
+
 impl AnalyticData {
     // Send request to RescueTime analytic data API endpoint, and return deserialized response.
     //TODO: switch out queries and format parameters with enum types
